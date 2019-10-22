a0febdf2f6cc84d29590a04c48e3029080bb9a98
SimpleJavaUser <50578518+SimpleJavaUser@users.noreply.github.com>
2019-10-22 13:23:48 +0300

    Update Main.java

--- classes/task4/Main.java
+++ classes/task4/Main.java
@@ -81,7 +81,7 @@ public class Main {
         }
     }
 
-
+// ошибка аналогичная предыдущему таску
     public static void descendingSortByTrainNumber(Train[] trains){
         int n;
         Train temp;
